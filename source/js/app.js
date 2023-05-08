d163ce872fdbabd436d54eda06ad9f6e191caec9
Lidiya <lidakonstantinova@mail.ru>
2023-05-08 21:52:32 +0400

    rework: swiper.js included

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,9 @@
-window.addEventListener('DOMContentLoaded', function () {
-
-});
+new Swiper(".swiper", {
+    navigation: {
+        nextEl: '.slider-btn-next',
+        prevEl: '.slider-btn-prev',
+    },
+    spaceBetween: 30,
+    slidesPerView: 4,
+    slidesPerGroup: 1,
+})
